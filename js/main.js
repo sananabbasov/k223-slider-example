457ee686efdbd79882fc8225fb085fb246465519
sananabbasov <sanan_abbasov@list.ru>
2023-12-07 14:43:58 +0400

    intiail

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// let screenWith = window.screen.width
+// const sliderItem = document.getElementsByClassName("slider-item")
+
+// let start = 0;
+// let startIndex = 0;
+// let sliderLength = sliderItem.length
+// setInterval(function () {
+//     StartSlider()
+// }, 4000)
+
+// function StartSlider() {
+//     startIndex++
+//     if (startIndex == sliderLength) {
+//         start = 0
+//         startIndex = 0
+//     }
+//     sliderItem[0].style.marginLeft = `-${start}px`
+//     start = start + 1920
+
+//     console.log((-start));
+// }
+
+
+var bes = [1,2,3,4,5,6,7,8,9]
+
+// slice(start,end)
+// var num=arr.slice(2)
+// var num=arr.slice(3,6)
+// var num= arr.slice(-5)
+// var num = arr.slice(2,-3)
+
+
+    var num=bes.slice(1,-1)
+console.log(num);
+
+//homework
+
