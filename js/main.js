93c868f5c62271123990114950a216c4d6d544c8
sananabbasov <sanan_abbasov@list.ru>
2023-12-07 15:59:16 +0400

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,37 +1,64 @@
-// let screenWith = window.screen.width
-// const sliderItem = document.getElementsByClassName("slider-item")
+let screenWith = window.screen.width
+const sliderItem = document.getElementsByClassName("slider-item")
 
-// let start = 0;
-// let startIndex = 0;
-// let sliderLength = sliderItem.length
+let start = screenWith;
+let startIndex = 0;
+let sliderLength = sliderItem.length
 // setInterval(function () {
 //     StartSlider()
 // }, 4000)
 
-// function StartSlider() {
-//     startIndex++
-//     if (startIndex == sliderLength) {
-//         start = 0
-//         startIndex = 0
-//     }
-//     sliderItem[0].style.marginLeft = `-${start}px`
-//     start = start + 1920
 
-//     console.log((-start));
+
+
+function StartSlider() {
+    startIndex++
+    if (startIndex == sliderLength) {
+        start = screenWith
+        startIndex = 0
+        console.log(startIndex);
+    }
+    sliderItem[0].style.marginLeft = `-${start}px`
+    start = start + 1920
+
+}
+
+
+function Next() {
+    startIndex++
+    if (startIndex == sliderLength) {
+        start = 0
+        startIndex = 0
+        console.log(start);
+
+    }
+    sliderItem[0].style.marginLeft = `-${start}px`
+    start = start + 1920
+    console.log(start);
+
+  
+
+}
+
+// function Prev() {
+//     startIndex--
+//     if (startIndex == 0) {
+//         start = 3840
+//         startIndex = sliderLength-1
+//         sliderItem[0].style.marginLeft = `-${start}px`
+//         console.log("plus"+start);
+
+//     } else {
+//         start = start - 1920
+//         sliderItem[0].style.marginLeft = `-${start}px`
+//         console.log(start);
+//     }
+
+
+
 // }
 
 
-var bes = [1,2,3,4,5,6,7,8,9]
-
-// slice(start,end)
-// var num=arr.slice(2)
-// var num=arr.slice(3,6)
-// var num= arr.slice(-5)
-// var num = arr.slice(2,-3)
 
 
-    var num=bes.slice(1,-1)
-console.log(num);
-
-//homework
 
